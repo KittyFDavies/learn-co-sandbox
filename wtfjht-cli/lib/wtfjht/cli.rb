8da344de3316cf4b6af9ed505d19bb97a210bb9a
Kitty Davies <kittyfdavies@gmail.com>
2018-11-11 03:35:56 +0000

    updates to README

--- wtfjht-cli/lib/wtfjht/cli.rb
+++
@@ -1,8 +0,0 @@
-require "wtfjht/cli/version"
-
-module Wtfjht
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- wtfjht-cli/lib/wtfjht/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Wtfjht
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
